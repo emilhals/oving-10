90af37174a5864dfa1be640829071a25c023b1e3
emilhals <113390306+emilhals@users.noreply.github.com>
2023-11-01 13:20:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import csv
+
+def lagre_data_til_liste():
+  with open('snoedybder_vaer_en_stasjon_dogn.csv', newline='') as file:
+    hentet_data = csv.reader(file, delimiter=' ', quotechar=';')
+    
+    data = []
+    
+    for line in hentet_data:
+      data.append(line)
+
+    return data
+
+def finn_snodager(liste):
+  for linje in liste:
+    print(linje)
+  
+  print(data)
+
 if __name__ == "__main__":
-  print('Hello world')
+  finn_snodager(lagre_data_til_liste())
