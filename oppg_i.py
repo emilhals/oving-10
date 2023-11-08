6e5252aaaf536003303b34fe1ea11019c9ae7161
thovahus <145988814+thovahus@users.noreply.github.com>
2023-11-08 15:20:44 +0100

    oppg_i.py

--- oppg_i.py
+++ oppg_i.py
@@ -1,9 +1,9 @@
 from main import lagre_data_til_liste
 import matplotlib.pyplot as plt
-import math
 
 def numberList(inlist):
     newlist = [inlist[x+1]-inlist[x] for x in range(0,len(inlist)-1)]
+    newlist.append(0)
     return newlist
 
 class snitt_maaned:
@@ -16,6 +16,8 @@ fullist = []
 maanedsnittlist =[] #snitt for hver måned i hvert år
 maaned = [1,2,3,4,5,6,7,8,9,10,11,12]
 templist = []
+tomlist = []
+aarsnitt = []
 feil = 0
 
 for year in lagre_data_til_liste():
@@ -35,10 +37,24 @@ for a in range(0,len(fullist)-1):
         except:feil+=1
 
 for d in range(0,len(maanedsnittlist)-1):
-    templist.append(maanedsnittlist[a].snitt)
+    templist.append(maanedsnittlist[d].snitt)
+    tomlist.append(d)
+for e in range(0,len(fullist)):
+    a = 0
+    for f in range(0,12):
+        try:a+=sum(fullist[e][f])
+        except:a+=0
+    aarsnitt.append(a)
 
-#plt.plot()
-plt.ylabel("gjennomsnittstemperatur")
-plt.xlabel("jan-des")
+maaneddifflist = numberList(templist)
+aar = [1980+a for a in range(0,len(lagre_data_til_liste()))]
+aardiff = numberList(aarsnitt)
+
+figure, axis = plt.subplots(2,2)
+axis[0,0].plot(tomlist,templist,"b")
+axis[1,0].plot(tomlist,maaneddifflist,"r")
+axis[0,1].plot(aar,aarsnitt,"b")
+axis[1,1].plot(aar,aardiff,"r")
+plt.show()
 
 print(f"{feil} feil")
